1184add14e2863894b9fe5d5768c8b727cc60b18
Anoop-cs011 <120665542+Anoop-cs011@users.noreply.github.com>
2025-03-24 12:25:40 +0530

    Create instanceStartup.sh

---
+++ instanceStartup.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt -y install git gunicorn3 python3-pip virtualenv
+git clone https://github.com/Anoop-cs011/VCC-assignment3.git
+cd VCC-assignment3
+virtualenv myProjectEnv
+source myProjectEnv/bin/activate
+pip install -r requirements.txt
