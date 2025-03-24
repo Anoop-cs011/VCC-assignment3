be541d76a3d8deae387391544ef98ef2b3536275
Anoop-cs011 <120665542+Anoop-cs011@users.noreply.github.com>
2025-03-24 08:24:15 +0530

    Update mainApp.py

--- mainApp.py
+++ mainApp.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, make_response
 import threading
 import multiprocessing
 from socket import gethostbyname, gethostname
-from random import random
 
 app = Flask(__name__)
 running = False
